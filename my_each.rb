5fb65248471077a0c7bf8aa83ce303b4789bd552
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-26 03:06:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)# put argument(s) here
   # code here
   i = 0
-  while i > arrays.length
-    yield(array[i])
+  while i < arrays.length
+    yield array [i]
     i = i + 1
   end
   array
